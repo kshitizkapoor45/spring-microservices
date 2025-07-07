9a8b9e170de7b2298fd1b4ef1e4c753070a4cf3d
kshitizkapoor45 <138753365+kshitizkapoor45@users.noreply.github.com>
2025-07-07 10:32:43 +0530

    Project setup

---
+++ kapoor/src/main/java/com/microservice/kapoor/KapoorApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.kapoor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KapoorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KapoorApplication.class, args);
+	}
+
+}
---
+++ kapoor/src/test/java/com/microservice/kapoor/KapoorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.kapoor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KapoorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
