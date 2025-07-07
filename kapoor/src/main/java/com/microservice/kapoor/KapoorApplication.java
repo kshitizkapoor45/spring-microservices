ddd587be5d5c9b135a5db404e94b6a35d4677185
kshitizkapoor45 <138753365+kshitizkapoor45@users.noreply.github.com>
2025-07-07 19:06:12 +0530

    Setting up department and server registry with zipkin

--- kapoor/src/main/java/com/microservice/kapoor/KapoorApplication.java
+++ kapoor/src/main/java/com/microservice/kapoor/KapoorApplication.java
@@ -2,8 +2,10 @@ package com.microservice.kapoor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class KapoorApplication {
 
 	public static void main(String[] args) {
